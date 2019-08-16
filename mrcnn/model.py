f1d8698616e20dd2aac86d7f9c0fa5321c7bb8a0
Nathan Bruzat <nathan.bruzat@mediamap.ai>
2019-08-16 17:58:21 +0200

    add verbose link to set_trainable

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2287,7 +2287,7 @@ class MaskRCNN():
 
     def train(self, train_dataset, val_dataset, learning_rate, epochs, layers,
               augmentation=None, custom_callbacks=None, no_augmentation_sources=None, use_multiprocessing=True,
-              gray_augmentation=False, verbose = 1):
+              gray_augmentation=False, verbose=1):
         """Train the model.
         train_dataset, val_dataset: Training and validation Dataset objects.
         learning_rate: The learning rate to train with
@@ -2365,7 +2365,7 @@ class MaskRCNN():
         # Train
         log("\nStarting at epoch {}. LR={}\n".format(self.epoch, learning_rate))
         log("Checkpoint Path: {}".format(self.checkpoint_path))
-        self.set_trainable(layers)
+        self.set_trainable(layers, verbose=verbose)
         self.compile(learning_rate, self.config.LEARNING_MOMENTUM)
 
         # Work-around for Windows: Keras fails on Windows when using

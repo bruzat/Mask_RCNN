8efbe589d497111084f3dcd3cf5a43ecaa661578
Nathan Bruzat <nathan.bruzat@mediamap.ai>
2019-08-16 17:57:02 +0200

    remove /4 on workers

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2376,7 +2376,7 @@ class MaskRCNN():
         elif use_multiprocessing == False:
             workers = 1
         else:
-            workers = multiprocessing.cpu_count() / 4
+            workers = multiprocessing.cpu_count()
 
         self.keras_model.fit_generator(
             train_generator,
